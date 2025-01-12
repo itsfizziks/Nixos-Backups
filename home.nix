41ec2d07fba6680f4424f932342f06a7fb3599e1
Itsfizziks <55935361+itsfizziks@users.noreply.github.com>
2025-01-12 02:18:42 -0500

    Add files via upload

--- home.nix
+++ home.nix
@@ -1,24 +1,53 @@
-{ pkgs, ... }:
-
-{
+{ pkgs, ...}:
+{ 
   home.username = "pouls0n";
   home.homeDirectory = "/home/pouls0n";
+
   home.packages = with pkgs; [
     steam
     bitwarden
     obsidian
-    zed-editor
     fastfetch
     kitty
     vlc
-  ];
+    brightnessctl
+    swww
+    oh-my-posh
+    nautilus
+    hyprcursor
+    pavucontrol
+    pulseaudio
+    vscode
+    #vscode-fhs
+    cmake
+    dolphin-emu
+    ntfs3g
+    fzf
+    tldr
+    bat
+    gimp
+    unzip
+    yazi
+    lsd
+    btop
+    spotify
+    libreoffice
+    nmap
+    wineWowPackages.stable
+    xterm
+    pywal
+    hyprshot
+    wireshark
+    blueman
+    dunst
+    discord
+   ];
 
   nixpkgs.config = {
     allowUnfree = true;
   };
 
-
   # Configure user settings
-  home.stateVersion = "24.11";  # Update this if needed
+  home.stateVersion = "25.05";  # Update this if needed
+  #24.11
 }
-
